9ad5f188b4c116a7a05ffbcaeee2400a46a249dc
Moa Junior <moacirfonsecajr@gmail.com>
2022-08-22 13:54:14 -0300

    Realizado bonus 1 2 e 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus2.js
@@ -0,0 +1,11 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
+let string = '';
+
+
+for (let j = 0; j < n; j++) {
+    string += '*';        
+    console.log(string);
+}
+
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus3.js
@@ -0,0 +1,26 @@
+
+
+let n = 7;
+let string = '';
+
+
+
+for (let i = 0; i < n/2; i++) {
+    
+    for (let j = 0; j < n; j++) {
+        if(i === (n-1)/2 || j === (n-1)/2){
+            string += '*';  
+        }else{
+            if(j+i < Math.floor(n/2) || j-i > Math.floor((n/2))){
+                string += ' ';
+            }else{
+                string += '*';
+            }
+        }        
+    }
+
+    string += '\n';
+}
+
+console.log(string);
+
