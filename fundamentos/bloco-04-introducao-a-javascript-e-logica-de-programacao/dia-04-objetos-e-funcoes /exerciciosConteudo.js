eeed0b63a01fe9c0b5ebae7fa2912a7730939f5b
Moa Junior <moacirfonsecajr@gmail.com>
2022-08-23 13:35:07 -0300

    Realizado exercicios funcoes

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes /exerciciosConteudo.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes /exerciciosConteudo.js
@@ -35,3 +35,104 @@ for (const key in car) {
 // Chave: manufacturer - Valor: Audi
 // Chave: year - Valor: 2020
 }
+
+// FUNÇÕES
+
+{
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+let soma = (a, b) => {
+    console.log(a + b);
+}
+let subtracao = (a, b) => {
+    console.log(a - b);
+}
+let multiplicacao = (a, b) => {
+    console.log(a * b);
+}
+let divisao = (a, b) => {
+    console.log(a / b);
+}
+let modulo = (a, b) => {
+    console.log(a % b);
+}
+
+soma(5, 7)
+subtracao(5, 7)
+multiplicacao(5, 7)
+divisao(5, 7)
+modulo(5, 7)
+
+// output:
+// 12
+// -2
+// 35
+// 0.7142857142857143
+// 5
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+let maiorNumero = (a, b) => {
+    if(a > b) console.log(a + ' é o maior número.');
+    else if(b > a) console.log(b + ' é o maior número.');
+    else console.log('Numeros iguais');
+}
+
+maiorNumero(10, 5)
+
+// output:
+// 10 é o maior número.
+
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+let maiorTresNumeros = (a, b, c) => {
+    if(a > b && a > c) console.log(a + ' é o maior número.');
+    else if(b > c) console.log(b + ' é o maior número.');
+    else console.log(c + ' é o maior número.');
+}
+
+maiorTresNumeros(1, 2, 3)
+
+// output:
+// 3 é o maior número.
+
+// Faça um programa que, dado um valor recebido como parâmetro, retorne "positive" se esse valor for positivo, "negative" se for negativo, e caso não seja nem positivo e nem negativo retorne "zero".
+let signalCheck = (n) => {
+    if (n > 0) console.log('Positive');
+    else if (n < 0) console.log('Negative');
+    else console.log('Zero');
+}
+
+signalCheck(15)
+signalCheck(-15)
+signalCheck(0)
+
+// output:
+// Positive
+// Negative
+// Zero
+
+// Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. 
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. 
+// Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+let isTriangle = (x, y, z) => {
+    if(x <= 0 || y <= 0 || z <= 0) {
+        console.log('Erro! Insira valores válidos');
+        return;
+    }
+    if(x + y + z === 180) console.log(true); 
+    else console.log(false);
+}
+
+isTriangle(120, 0, -60)
+isTriangle(120, 45, 15)
+isTriangle(120, 60, 15)
+
+// output:
+// Erro! Insira valores válidos
+// true
+// false
+}
