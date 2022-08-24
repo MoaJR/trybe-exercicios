49729e0f40b99a59d82a999ed7d0cab4aa1cfdef
Moa Junior <moacirfonsecajr@gmail.com>
2022-08-24 10:18:08 -0300

    Realizado Bonus 2 do bloco 4.4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes /bonus2.js
@@ -0,0 +1,48 @@
+let mostRepetitive = (arrayUnsorted) => {
+    let numeroRepeticoes = 0;
+    let numeroRepetido = 0;
+
+    let obj = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        let count = 0;
+
+        for (let j = 0; j < array.length; j++) {
+            if(array[i] === array[j]){
+                count += 1;
+            }
+        }  
+        if(!obj.includes(array[i])){
+            
+            numeroRepeticoes = count;
+            numeroRepetido = array[i];
+
+            obj.push(numeroRepeticoes, numeroRepetido);
+        }   
+        
+    }
+    console.log(obj);
+}
+
+
+array = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva', 'Chocolate',
+];
+mostRepetitive(array)
